c3163b57d73fe9b257a3bb02a9162a0e159df32b
vitt_gchsantos <gabriel.santos@vittude.com>
2022-10-28 16:26:35 -0300

    feat(authentication): create BearerAuthentication for user Token

--- jusbrasil_challenge/jusbrasil_challenge/settings.py
+++ jusbrasil_challenge/jusbrasil_challenge/settings.py
@@ -115,3 +115,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "jusbrasil_challenge.authentications.BearerAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAdminUser"),
+}
