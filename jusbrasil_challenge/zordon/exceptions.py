ff11461142473f9f8629191ae92ff4dfc6d6b83f
vitt_gchsantos <gabriel.santos@vittude.com>
2022-10-31 18:48:19 -0300

    feat(zordon): create zordon exceptions

---
+++ jusbrasil_challenge/zordon/exceptions.py
@@ -0,0 +1,22 @@
+import logging
+import json
+
+from jusbrasil_challenge.exceptions import BaseException
+from jusbrasil_challenge.messages import ErrorMessage
+
+
+logg = logging.getLogger("zordon.exceptions")
+
+
+class MissingValueException(BaseException):
+    name: str = "MissingValueError"
+    logger: logging.Logger = logg
+
+
+class UnsupportedCNJException(BaseException):
+    name: str = "CNJUnsupportedError"
+    logger: logging.Logger = logg
+
+    def build_message(self, cnj: str) -> dict:
+        message: str = f"CNJ '{cnj}' is not supported by the application"
+        return json.loads(ErrorMessage(description=message, type=self.name).to_json())
