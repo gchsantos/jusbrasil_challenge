3ff564877ac0e46524faef3267adbcc989b1a21d
vitt_gchsantos <gabriel.santos@vittude.com>
2022-10-31 18:58:04 -0300

    feat(zordon): create refresh_lawsuit field

--- jusbrasil_challenge/zordon/models.py
+++ jusbrasil_challenge/zordon/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class BatchGenerator(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     cnj = models.CharField(max_length=25)
+    refresh_lawsuit = models.BooleanField(default=False)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="solicitation"
     )
