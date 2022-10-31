fddf19b2c5c2d083e10cff980fab0f755d20b200
vitt_gchsantos <gabriel.santos@vittude.com>
2022-10-31 18:47:43 -0300

    feat: create base exception

---
+++ jusbrasil_challenge/jusbrasil_challenge/exceptions.py
@@ -0,0 +1,22 @@
+import logging
+import json
+
+from jusbrasil_challenge.messages import ErrorMessage
+
+logg = logging.getLogger("jusbrasil_challenge.exceptions")
+
+
+class BaseException(Exception):
+    name: str = "BaseError"
+    logger: logging.Logger = logg
+
+    def __init__(self, *args: object, **kwargs) -> None:
+        self.message = self.build_message(*args, **kwargs)
+        self.log()
+        super().__init__(self.message)
+
+    def build_message(self, message) -> dict:
+        return json.loads(ErrorMessage(description=message, type=self.name).to_json())
+
+    def log(self, *args, message=None, **kwargs):
+        self.logger.exception(message or self.message)
