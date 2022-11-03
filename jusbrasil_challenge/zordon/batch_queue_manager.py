747b4bf29ed8a7e1729b914462209b4d8baea27f
vitt_gchsantos <gabriel.santos@vittude.com>
2022-11-03 20:07:59 -0300

    refactor: remove messa queue manager

--- jusbrasil_challenge/zordon/batch_queue_manager.py
+++
@@ -1,7 +0,0 @@
-from jusbrasil_challenge.mq.manager import MQManager
-
-
-class BatchQueueManager(MQManager):
-    def send_batch_message(self, message: str, uf: str):
-        queue = f"batch_{uf}"
-        self.send_message(message, queue)
