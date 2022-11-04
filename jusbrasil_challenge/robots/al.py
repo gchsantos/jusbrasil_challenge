4a0e5db2f2eed5e62134612d1ce3708eca9455f2
vitt_gchsantos <gabriel.santos@vittude.com>
2022-11-04 05:09:59 -0300

    refactor(robots): refact robots.al capture task to adapt in EsajCaptureRobot

--- jusbrasil_challenge/robots/al.py
+++ jusbrasil_challenge/robots/al.py
@@ -3,28 +3,22 @@ from celery.utils.log import get_task_logger
 
 from .core.esaj_capture_robot import EsajCaptureRobot
 from .core.task_base import BaseTaskWithRetry
-from .core.exceptions import BaseCaptureException
-
 
 logger = get_task_logger(__name__)
 
 
 class RobotAL(EsajCaptureRobot):
-    url: str = "https://www2.tjal.jus.br/cpopg/open.do"
+    url = "https://www2.tjal.jus.br/cpopg/open.do"
+    search_url = "https://www2.tjal.jus.br/cpopg/show.do?processo.numero="
 
     def __init__(self, **kwargs):
-        super().__init__(url=self.url)
-
-    def capture_pipeline(self, cnj: str, batch_line_id: str, uid: str):
-        try:
-            self.search_lawsuit_by_cnj(cnj)
-            return f"Hello ALAGOAS! {cnj, batch_line_id, uid}"
-
-        except (Exception) as e:
-            raise BaseCaptureException(e)
+        super().__init__(self.url, self.search_url)
 
 
 @shared_task(bind=True, base=BaseTaskWithRetry)
 def capture_task(self, cnj: str, batch_line_id: str, uid: str):
+    from zordon.models import BatchLine
+
     self.update_state(state="STARTED", meta={"user_id": uid})
-    RobotAL().capture_pipeline(cnj, batch_line_id, uid)
+    status, lawsuit_data = RobotAL().capture_pipeline(cnj)
+    BatchLine.objects.get(id=batch_line_id).update_status(status)
