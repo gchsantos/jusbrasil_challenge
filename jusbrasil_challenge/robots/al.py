6d532fd749f6acf887cf672101190863383a3027
vitt_gchsantos <gabriel.santos@vittude.com>
2022-11-05 21:25:04 -0300

    feat(robots): handle errors in robots

--- jusbrasil_challenge/robots/al.py
+++ jusbrasil_challenge/robots/al.py
@@ -1,13 +1,10 @@
 from celery import shared_task
-from celery.utils.log import get_task_logger
 from django.db import transaction
 
 from .core.esaj_capture_robot import EsajCaptureRobot
 from .core.task_base import BaseTaskWithRetry
 from .core.dataclasses import RefinedLawsuitData
 from zordon.constants import LINE_STATUS
-
-logger = get_task_logger(__name__)
 
 
 class RobotAL(EsajCaptureRobot):
@@ -33,27 +30,37 @@ def capture_task(self, cnj: str, batch_line_id: str, uid: str):
     from zordon.models import BatchLine, LawsuitGenerator
 
     self.update_state(state="STARTED", meta={"user_id": uid})
-    status, lawsuit_datas = RobotAL().capture_pipeline(cnj)
 
-    if status == LINE_STATUS.SUCCESS:
-        with transaction.atomic():
-            for lawsuit_data in lawsuit_datas:
-                instance = lawsuit_data["instance"]
-                refinned_lawsuit: RefinedLawsuitData = lawsuit_data["capture_response"]
-                lawsuit_generator = LawsuitGenerator.objects.create(
-                    batch_line_id=batch_line_id,
-                    instance=instance,
-                    value=refinned_lawsuit.value,
-                    lawsuit_class=refinned_lawsuit.lawsuit_class,
-                    subject=refinned_lawsuit.subject,
-                    distribution=refinned_lawsuit.distribution,
-                    area=refinned_lawsuit.area,
-                    judge=refinned_lawsuit.judge,
-                )
+    try:
+        status, lawsuit_datas = RobotAL().capture_pipeline(cnj)
 
-                lawsuit_generator.generate(
-                    refinned_lawsuit.progress_table,
-                    refinned_lawsuit.concerned_parties_table,
-                )
+        if status == LINE_STATUS.SUCCESS:
+            with transaction.atomic():
+                for lawsuit_data in lawsuit_datas:
+                    instance = lawsuit_data["instance"]
+                    refinned_lawsuit: RefinedLawsuitData = lawsuit_data[
+                        "capture_response"
+                    ]
+                    lawsuit_generator = LawsuitGenerator.objects.create(
+                        batch_line_id=batch_line_id,
+                        instance=instance,
+                        value=refinned_lawsuit.value,
+                        lawsuit_class=refinned_lawsuit.lawsuit_class,
+                        subject=refinned_lawsuit.subject,
+                        distribution=refinned_lawsuit.distribution,
+                        area=refinned_lawsuit.area,
+                        judge=refinned_lawsuit.judge,
+                    )
 
-        BatchLine.objects.get(id=batch_line_id).update_status(status)
+                    lawsuit_generator.generate(
+                        refinned_lawsuit.progress_table,
+                        refinned_lawsuit.concerned_parties_table,
+                    )
+
+            BatchLine.objects.get(id=batch_line_id).update_status(status)
+    except Exception as e:
+        if self.request.retries == self.max_retries:
+            BatchLine.objects.get(id=batch_line_id).update_status(
+                LINE_STATUS.ERROR, str(e)
+            )
+        raise
