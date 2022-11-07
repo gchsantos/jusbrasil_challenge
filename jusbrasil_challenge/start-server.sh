94fd07cb0c2386c99e9dc23a5931dfb19342ab22
vitt_gchsantos <gabriel.santos@vittude.com>
2022-11-07 03:26:15 -0300

    feat: remove adminer and adjust healthcheck and test run routine

--- jusbrasil_challenge/start-server.sh
+++ jusbrasil_challenge/start-server.sh
@@ -5,6 +5,11 @@ NAME="jusbrasil_challenge"
 DJANGO_WSGI_MODULE=jusbrasil_challenge.wsgi
 NUM_WORKERS=3
 
+if ! python manage.py test --k; then
+    echo '[ ALERT: Application not pass in the tests. ]'
+    exit 1
+fi
+
 python manage.py makemigrations zordon
 python manage.py migrate
 
